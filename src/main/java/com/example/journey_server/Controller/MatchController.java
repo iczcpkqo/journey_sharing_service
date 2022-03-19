7dcf5e1172d74f6bf8cbb888e1a1403b3fb33ad4
xizheng <xizheng@tcd.ie>
2022-03-19 23:23:17 +0000

    udpate redis APIs

--- src/main/java/com/example/journey_server/Controller/MatchController.java
+++ src/main/java/com/example/journey_server/Controller/MatchController.java
@@ -3,12 +3,9 @@ package com.example.journey_server.Controller;
 import com.example.journey_server.Service.MatchService;
 import com.example.journey_server.entity.Peer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
